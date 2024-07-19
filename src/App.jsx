5f05a95e9155294e5d0e2b1042e8330b42295e6c
vishwaraja21 <vishwaraja.rr@gmail.com>
2024-07-19 14:34:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StdinfoComponents from './Components/StdinfoComponents/StdinfoComponents'
+
+function App() {
+  return (
+    <div>
+      <StdinfoComponents/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/StdinfoComponents/StdinfoComponents.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import '../StdinfoComponents/StdinfoComponents.css';
+import img from '../../assets/image/img1.jpeg';
+
+const StdinfoComponents = () => {
+  const [isvisible,setIsvisible] = useState(false)
+  const togglevisibility = () => {
+    setIsvisible(!isvisible)
+  }
+  
+  
+    return (
+    <div className="container">
+      <div className="card">
+        <div className="icon">
+           
+          <img src={img} alt="Eye Icon" />
+        </div>
+        <p><h4>Name: Vishwaraja R</h4></p>
+        <p>Registration No: 212221220060</p>
+        <button className="toggle-button" onClick={togglevisibility}>
+          {isvisible ? 'Hide Details' : 'Show Details'}
+        </button>
+        <div className={`additional-content ${isvisible ? 'visible' : 'hidden'}`}>
+            <p>I am currently pursuing B.Tech(IT) at Saveetha Engineering College</p>
+        </div>
+    </div>
+    </div>
+  );
+};
+
+export default StdinfoComponents;
